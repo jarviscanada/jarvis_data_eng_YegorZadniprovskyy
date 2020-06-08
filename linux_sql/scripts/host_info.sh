129b4a0db167363cc401694f5e6677564edc5156
Yegor Zadniprovskyy <yegor.zadniprovskyy@gmail.com>
2020-06-08 17:30:16 +0000

    Assigned CLI arguments and checked for number pf params in host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,14 @@
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ $? -ne 5 ]; then
+  echo "Error: Illegal number of parameters. Usage: ./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password
+"
+  exit 1
+fi
+
+
+
