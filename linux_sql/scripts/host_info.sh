a6124572c6b21296b49457b0e94dff3f2c5adeb6
Yegor Zadniprovskyy <yegor.zadniprovskyy@gmail.com>
2020-06-09 23:23:33 +0000

    Created a helper for howt_info

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -15,20 +15,29 @@ fi
 lscpu_out=`lscpu`
 meminfo_out=`cat /proc/meminfo`
 
+# Helper function for extracting data of interest (use the same pipeline of cmd line tools)
+function echo_egrep_awk_xargs {
+  cmd_out=$1
+  egrep_arg=$2
+  awk_arg=$3
+  echo "$cmd_out" | egrep "$egrep_arg" | awk "$awk_arg" | xargs
+}
 
 hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}')
-cpu_model=$(echo "($lscpu_out)" | egrep "^Model name:" | awk '{for (i=3; i<NF+1; i++) print $i}' | xargs)
-cpu_mhz=$(echo "($lscpu_out)" | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache=$(echo "($lscpu_out)" | egrep "^L2 cache:" | awk '{print $3}' | xargs | sed 's/[^0-9]*//g')
-total_mem=$(echo "($meminfo_out)" | egrep "MemTotal:" | awk '{print $2}' | xargs)
+
+cpu_number=$(echo_egrep_awk_xargs "$lscpu_out" "^CPU\(s\):" '{print $2}')
+cpu_architecture=$(echo_egrep_awk_xargs "$lscpu_out" "^Architecture:" '{print $2}')
+cpu_model=$(echo_egrep_awk_xargs "$lscpu_out" "^Model name:" '{for (i=3; i<NF+1; i++) print $i}')
+cpu_mhz=$(echo_egrep_awk_xargs "$lscpu_out" "^CPU MHz:" '{print $3}')
+l2_cache=$(echo_egrep_awk_xargs "$lscpu_out" "^L2 cache:" '{print $3}' | sed 's/[^0-9]*//g')
+total_mem=$(echo_egrep_awk_xargs "$meminfo_out" "MemTotal:" '{print $2}')
 
 # current timestamp in `2019-11-26 14:40:19` format
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
 # Insert a row into host_info table
 sql_insert="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES  ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $l2_cache, $total_mem, '$timestamp')"
+echo "$sql_insert"
 PGPASSWORD=$psql_password psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$sql_insert"
 
 exit $?
