2dfb8055145d809b94464f272def3afed7000d28
Yegor Zadniprovskyy <yegor.zadniprovskyy@gmail.com>
2020-06-09 22:13:03 +0000

    Refactored code so that inserting using subquery for host_id retrieval.

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -15,15 +15,21 @@ fi
 df_out=`df -BM /`
 vmstat_out=`vmstat -t --unit M`
 
+hostname=$(hostname -f)
+
 # current timestamp in `2019-11-26 14:40:19` format
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-host_id=1
 memory_free=$(echo "$vmstat_out" | awk '{print $4}' | egrep -o '[0-9]*')
 cpu_idle=$(echo "$vmstat_out" | awk '{print $15}' | egrep -o '[0-9]*')
 cpu_kernel=$(echo "$vmstat_out" | awk '{print $14}' | egrep -o '[0-9]*')
 disk_io=$(vmstat -d | awk '{print $10}' | egrep -o '[0-9]*')
 disk_available=$(echo "$df_out" | awk '{print $4}' | egrep -o '[0-9]*')
 
+# Precondition: corresponding host_info row exists
+sql_select="SELECT id FROM host_info WHERE hostname='$hostname'"
+
 # Insert a row into host_usage table
-sql_insert="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES ('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernel, '$disk_io', $disk_available)"
+sql_insert="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES ('$timestamp', ($sql_select), $memory_free, $cpu_idle, $cpu_kernel, '$disk_io', $disk_available)"
 PGPASSWORD=$psql_password psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$sql_insert"
+
+exit $?
