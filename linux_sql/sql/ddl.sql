f1cb6f39d1c540bb83f524011ee06f6759cd0e44
Yegor Zadniprovskyy <yegor.zadniprovskyy@gmail.com>
2020-06-09 14:06:30 +0000

    Made host_name column non unique.

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
   id SERIAL NOT NULL,
-  hostname VARCHAR NOT NULL UNIQUE,
+  hostname VARCHAR NOT NULL,
   cpu_number INT NOT NULL,
   cpu_architecture VARCHAR NOT NULL,
   cpu_model VARCHAR NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
   L2_cache INT NOT NULL,
   total_mem INT NOT NULL,
   "timestamp" TIMESTAMP NOT NULL,
-  PRIMARY KEY id
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
